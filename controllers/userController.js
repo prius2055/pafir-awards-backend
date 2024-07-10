1d370ee6a8accd59b7e0b11f41dcbd452a8ced02
prius2055 <prius2055@gmail.com>
2024-07-10 16:15:07 +0100

    Add session storage

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,7 @@ const signInUser = async (req, res) => {
 
     const token = jwt.sign({ userId: user._id }, jwtSecret);
     res.cookie('token', token, { httpOnly: true });
-    // req.session.token = token;
+    req.session.token = token;
     res.status(200).json({ user, code: 200, msg: 'Successfully logged in' });
   } catch (error) {
     res.status(500).json({ code: 500, msg: error.message });
