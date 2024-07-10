4bd833829bb77f5900704ffd00cf47fa51a4f7aa
prius2055 <prius2055@gmail.com>
2024-07-10 16:27:23 +0100

    Remove session storage

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,8 +17,12 @@ const signInUser = async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user._id }, jwtSecret);
-    res.cookie('token', token, { httpOnly: true });
-    req.session.token = token;
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
     res.status(200).json({ user, code: 200, msg: 'Successfully logged in' });
   } catch (error) {
     res.status(500).json({ code: 500, msg: error.message });
